d2bf249c2f33326387fbd0ee3b35a516f268205d
Kyler Ross <Kyler@Admins-MBP.wireless.newschool.edu>
2018-03-09 14:20:31 -0500

    testfinal commit

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name = 'home'),
-    path('about/', views.aboutpageview.as_view(), name = 'about')
+    path('about/', views.aboutpageview.as_view(), name = 'about'),
+    path('Project 1/', views.project1pageview.as_view(), name = 'Project 1'),
+    path('Project 2/', views.project2pageview.as_view(), name = 'Project 2'),
+    path('Project 3/', views.project3pageview.as_view(), name = 'Project 3')
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,11 +6,18 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 # Create your views here.
-def homepageview(request):
-    return HttpResponse("<h3>It's So Wonderful to see you Kyler!</h3>")
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 class aboutpageview(TemplateView):
     template_name = 'about.html'
+
+class project1pageview(TemplateView):
+    template_name = 'Week1_ProjectA.html'
+
+class project2pageview(TemplateView):
+    template_name = 'Week2_ProjectB.html'
+
+class project3pageview(TemplateView):
+    template_name = 'Week3_ProjectC.html'
